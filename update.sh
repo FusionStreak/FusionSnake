d64e4bb1094d83247f5041caad6e991598df35de
Sayfullah Eid <sayfullaheid@gmail.com>
2024-06-16 13:34:48 -0400

    chore: Update Docker Compose commands in update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Rebuild the container
-docker-compose build
+docker compose build
 
 # Stop and remove the existing containers
-docker-compose down
+docker compose down
 
 # Launch the containers
-docker-compose up -d
+docker compose up -d
